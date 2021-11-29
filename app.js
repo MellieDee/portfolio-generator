17194497a9bdc954829c619bd2aae883b951bd34
Mellie Dee <89175650+MellieDee@users.noreply.github.com>
2021-11-28 19:02:42 -0800

    refaactor to create promisse chain and add gerenate site.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const fs = require('fs');
+// const fs = require('fs'); - using generate site w/promises instead
+// const generateSite = require('./utils/generate-site.js'); aka ->
 const inquirer = require('inquirer');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template');
+
 
 const promptUser = () => {
   return inquirer.prompt([
@@ -126,14 +129,58 @@ Add a New Project
     });
 };
 
+
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
+    return generatePage(portfolioData)
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML)
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse)
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse)
+  })
+.catch (err => {
+  console.log(err);
+});
 
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
 
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
-  });
+
+
+
+
+
+
+
+
+
+
+
+
+//*********  Before refactor to pormises it was nested callbacks aka Pyramid of Doom aka Callback Hell
+// promptUser()
+//   .then(promptProject)
+//   .then(portfolioData => {
+//     const pageHTML = generatePage(portfolioData);
+
+//     fs.writeFile('./dist/index.html', pageHTML, err => {
+//       if (err) {
+//         console.log(err);
+//         return;
+//       }
+//       console.log('Page created! Check out index.html in this directory to see it!');
+
+//       fs.copyFile('./src/style.css', './dist/style.css', err => {
+//         if (err) {
+//           console.log(err);
+//           return;
+//         }
+//         console.log('style sheet successful');
+//       })
+//     });
+//   });
